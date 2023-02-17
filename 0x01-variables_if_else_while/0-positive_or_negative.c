40088ed05be5924d9c7ae8789de98f9290d06753
Osama2004o <osamaeslam2013@gmail.com>
2023-02-17 06:24:38 -0800

    i

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n;   
+srand(time(0));  
+n = rand() - RAND_MAX / 2;
+if(n<0)
+{
+printf('%i is negative',n);
+}
+else if (n>0)
+{
+printf('%i is positive',n);
+}
+else
+{
+printf('%i is zero',n);
+}
+
+}
