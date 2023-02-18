0103a805059c98f01d73b76310a406eb768f1d5b
Osama2004o <93222729+Osama2004o@users.noreply.github.com>
2023-02-18 13:30:30 +0200

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, ");
+printf("15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, ");
+printf("29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, ");
+Printf("49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89\n");
+
+return (0);
+}
