4f613aa8ad1e1d92f31065e8c83feb9103e29fcd
Osama2004o <93222729+Osama2004o@users.noreply.github.com>
2023-02-18 13:06:34 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("abcdefghijklmnopqrstuvwxyz\n");
+
+return (0);
+}
