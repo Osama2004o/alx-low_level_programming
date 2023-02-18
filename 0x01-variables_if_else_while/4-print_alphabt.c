ca34ae4230c828968146b19c46aba2158442ec6f
Osama2004o <93222729+Osama2004o@users.noreply.github.com>
2023-02-18 13:12:24 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+printf("abcdfghijklmnoprstuvwxyz\n");
+
+return (0);
+}
