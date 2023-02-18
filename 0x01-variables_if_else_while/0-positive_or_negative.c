64a7920068fdf0075c76ae4dbfdaef0b418b4a73
Osama2004o <93222729+Osama2004o@users.noreply.github.com>
2023-02-18 13:01:40 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,21 +7,21 @@
 int main(void)
 {
 int n;   
-srand (time(0));  
-n = rand () - RAND_MAX / 2;
+srand(time(0));  
+n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf ("%i is negative", n);
+printf("%i is negative", n);
 }
 else if (n > 0)
 {
-printf ("%i is positive", n);
+printf("%i is positive", n);
 }
 else
 {
-printf ("%i is zero", n);
+printf("%i is zero", n);
 }
-printf ("\n");
-return 0;
+printf("\n");
+return (0);
 
 }
