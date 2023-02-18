ddfae349098a97f97d1752a70108773b702183a5
Osama2004o <93222729+Osama2004o@users.noreply.github.com>
2023-02-18 13:20:49 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n");
+
+return (0);
+}
