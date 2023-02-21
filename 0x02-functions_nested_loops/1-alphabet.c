70fbdb1804f5d1fbacca5dd8763fb27ed6a6fb5c
Osama2004o <osamaeslam2013@gmail.com>
2023-02-21 10:44:24 -0800

    h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ *print_alphabet - Prints the lowercase alphabet followed by a new line
+ **/
 void print_alphabet(void)
 {
-char letter = 'a';    
+char letter = 'a';
 while (letter <= 'z')
 {
 putchar(letter);
