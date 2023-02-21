b95ff0cbbdadb8e688c08abb682235e2a786cefe
Osama2004o <osamaeslam2013@gmail.com>
2023-02-21 10:25:26 -0800

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,8 @@
 /**
  * main - Entry point
  *
+ * print_alphabet
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
@@ -12,7 +14,7 @@ int i;
 for (i = 97; i < 123; i++)
 {
 putchar(i);
-if(i==122)
+if (i == 122)
 printf("\n");
 }
 }
