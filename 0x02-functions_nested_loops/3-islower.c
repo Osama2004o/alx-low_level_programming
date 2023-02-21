2473740d517f789d2fd5c4ef9aea2e5358291f34
Osama2004o <osamaeslam2013@gmail.com>
2023-02-21 11:36:22 -0800

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ *_islower - lower case letter
+ **/
+int _islower(int c)
+{
+int c;
+if (c >= 'a' && c <= 'z')
+return (1);
+}
+else
+{
+return (0);
+}
+}
