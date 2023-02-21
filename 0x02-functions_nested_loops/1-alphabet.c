0a27b9cefb0e8a9bbfed046940ad2d53cd1ee049
Osama2004o <osamaeslam2013@gmail.com>
2023-02-21 03:20:23 -0800

    h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 int i;
@@ -5,4 +13,9 @@ for (i = 97; i < 123; i++)
 {
 putchar(i);
 }
+}
+int main(void)
+{
+print_alphabet();
+return (0);
 }
