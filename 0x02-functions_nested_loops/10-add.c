738aec890d3e7f4b667a093a9f18e0507a08d95c
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 11:41:55 -0800

    i

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,11 +4,13 @@
 
 /**
  * add- Prints the 9 times table, starting with 0
+ * @n: integer
+ * @a: integer
  *
- * Return: @n + @a 
+ * Return: @n + @a
  **/
 int add(int n, int a)
 {
-int n, a;
-return(n+a);
+int c, b;
+return (c + b);
 }
