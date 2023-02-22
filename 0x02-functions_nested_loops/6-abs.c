b905104c1815f69e8497d46a62b5a83b34222665
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 05:23:25 -0800

    s

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - for abs value
+ *
+ * Return abs
+ **/
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else if (n == 0)
+return (0);
+else
+return (n);
+}
