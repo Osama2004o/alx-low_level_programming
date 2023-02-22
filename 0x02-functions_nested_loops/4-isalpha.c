6e6a9be2c2fd821fb851524cf704dfca892f5c21
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 04:32:12 -0800

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha() - check if letter alpha
+ *
+ **/
+int _isalpha(int c)
+{
+int _islower(int c)
+{
+if (c >= 'A' && c <= 'A')
+return (1);
+else
+return (0);
+}
+}
