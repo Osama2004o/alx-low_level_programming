82ac59cf60a1a58a86b742910bf9cfa0b03e6893
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 11:19:23 -0800

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ **/
+void print_jack_bauer(void)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf(" %02d:%02d\n", hour, minute);
+}
+}
+}
