2b0c34dcce1e825f09e0a5ebcc90e3f3f5a6cd62
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 11:05:09 -0800

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,8 @@
 void print_alphabet(void);
-<<<<<<< HEAD
-=======
 void print_alphabet_x10(void);
 int _islower(int c);
 int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-<<<<<<< HEAD
 int print_last_digit(int);
-=======
->>>>>>> 61c699304718b24ce60b471ebde2e9e3b011f390
->>>>>>> 7f10f9ed8dec7b9778229283beefac7cc8604765
