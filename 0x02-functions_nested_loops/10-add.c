3fcb51fb12a57dc850d691af73ffa19fa8d8e3ea
Osama2004o <osamaeslam2013@gmail.com>
2023-02-22 11:39:04 -0800

    t push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * add- Prints the 9 times table, starting with 0
+ *
+ * Return: @n + @a 
+ **/
+int add(int n, int a)
+{
+int n, a;
+return(n+a);
+}
