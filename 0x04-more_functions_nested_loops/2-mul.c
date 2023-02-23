f66e818132abdbd1aba59d31345e439901e9880a
Osama2004o <osamaeslam2013@gmail.com>
2023-02-23 04:58:00 -0800

    i

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - returns 1 if upper
+ * @a: first integer
+ * @b: second integer
+ * Return: the multiplication of a * b
+ **/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
