bedcc30a7bf0b4177183d232bdeae6dd7c117bbb
Osama2004o <osamaeslam2013@gmail.com>
2023-02-23 05:07:41 -0800

    i

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - returns 1 if upper
+ * Return: prints the numbers from 0 to 9
+ **/
+
+void print_numbers(void)
+{
+int i;
+for (i = 48 ; i <= 57 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
