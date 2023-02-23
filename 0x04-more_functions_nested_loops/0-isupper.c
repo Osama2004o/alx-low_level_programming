f9b9c8e0c6191fc4b73b595ab8089ce327f896ba
Osama2004o <osamaeslam2013@gmail.com>
2023-02-23 04:48:19 -0800

    i

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _isupper - returns 1 if upper
+ * @c: the integer
+ * Return: 1 if upper else return 0
+ **/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
